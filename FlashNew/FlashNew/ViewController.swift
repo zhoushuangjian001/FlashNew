a4ade1574d8a21e832b1afca33797d3a6ad42eb8
shuangjian.zhou <zhoushuangjian@algento.com>
2019-05-26 15:14:57 +0800

    项目创建

---
+++ FlashNew/FlashNew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashNew
+//
+//  Created by 周双建 on 2019/5/26.
+//  Copyright © 2019 周双建. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
